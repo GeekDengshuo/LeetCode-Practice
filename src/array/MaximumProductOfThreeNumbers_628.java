a7006ac28b36f3360cce0f3c5efab0e828703fbe
GeekDengshuo <1029456214@qq.com>
2021-01-20 23:33:02 +0800

    array linear scan

---
+++ src/array/MaximumProductOfThreeNumbers_628.java
@@ -0,0 +1,55 @@
+package src.array;
+
+import java.util.Arrays;
+
+/**
+ * @Author deng shuo
+ * @Date 2021/1/20 21:52
+ * @Version 1.0
+ */
+public class MaximumProductOfThreeNumbers_628 {
+
+    // O(NLogN)
+    public int maximumProduct(int[] nums) {
+
+        Arrays.sort(nums);
+
+        int len = nums.length;
+
+
+        return Math.max(nums[0] * nums[1] * nums[len - 1], nums[len - 3] * nums[len - 2] * nums[len - 1]);
+
+    }
+
+    public int maximumProductLinearScan(int nums[]){
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int max3 = Integer.MIN_VALUE;
+
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+
+        for(int num : nums){
+
+            if(num < min1){
+                min2 = min1;
+                min1 = num;
+            }else if( num < min2){
+                min2 = num;
+            }
+
+            if(num > max1){
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            }else if(num > max2){
+                max3 = max2;
+                max2 = num;
+            }else if(num > max3){
+                max3 = num;
+            }
+        }
+
+        return Math.max(min1 * min2 * max1,max3 * max2 * max1);
+    }
+}
